14ec4dddb4797ebb8598b52e4cf8d9cd53c41ba7
a2453540 <a2453540@gmail.com>
2023-07-01 19:58:41 +0800

    model的常用技巧

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     use HasFactory;
+    // 白名單，只有那些可以更新
+    // protected $fillable = ['quantity'];
+    
+    // 黑名單
+    protected $guarded = ['']; //參數皆可改
+
+    // 隱藏欄位
+    protected $hidden = ['updated_at'];
+    // 新增欄位
+    protected $appends = ['current_price'];
+    // 設定current_price
+    public function getCurrentPriceAttribute() {
+        return $this->quantity * 10 ;
+    }
 }
