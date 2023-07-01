162a3a4e35f62dbeea588ae3a215b52b41c08fac
a2453540 <a2453540@gmail.com>
2023-07-01 20:44:59 +0800

    model的關聯性

--- app/Http/Controllers/CartItemController.php
+++ app/Http/Controllers/CartItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\CartItem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -15,11 +15,19 @@ class CartItem extends Model
     protected $guarded = ['']; //參數皆可改
 
     // 隱藏欄位
-    protected $hidden = ['updated_at'];
+    // protected $hidden = ['updated_at'];
     // 新增欄位
     protected $appends = ['current_price'];
     // 設定current_price
     public function getCurrentPriceAttribute() {
         return $this->quantity * 10 ;
+    }
+
+    // belongsTo  單筆資料
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+    public function cart() {
+        return $this->belongsTo(Cart::class);
     }
 }
